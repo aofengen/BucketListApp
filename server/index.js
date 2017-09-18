76a38b588707a94eb0b98bc92e7deff2baf028de
Aaron Ofengender <aofengen@gmail.com>
2017-09-18 10:04:04 -0400

    end of module 2

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json({ type: '*/*'}));
+
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+console.log('Server listening on ' + port);
